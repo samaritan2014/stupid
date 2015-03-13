358a8c42d00c013eb3a1dfc97efe3e7eb24bdab8
samaritan2014 <samaritan2014@users.noreply.github.com>
2015-03-13 01:00:18 -0400

    Update lesson1.py

--- lesson1.py
+++ lesson1.py
@@ -7,19 +7,19 @@ gender = raw_input("What is your gender?")
 print "Hello %s! Nice to meet you!"%(name)
 
 
-if age == int:
-	ageGuess = 2015 - age
+if age < 2015:
+	ageGuess = (2015 - age)
 else:
-	print "Please enter a number for the year Bitch!"
+	print "Please enter a valid number for the year Bitch!"
 
 print "I guess you are str(ageGuess) years old."
 
 if gender.lower() == "male":
-	print ("So you have a penis! Haha!")
+	print ("So you have a penis! :) ")
 elif gender.lower() == "female":
 	print ("Mmmmh...will I ever get to eat your cookie? #justThinking")
 else:
-	print ("It's ok to be transgender too. After all I'm just a computer with no feelings! :) ")	
+	print ("It's ok to have no gender too. After all I'm just a computer with no feelings! :) ")	
 
 
 
